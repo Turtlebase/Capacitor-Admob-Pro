338df57a6ab92f2f03924af3ad1b65fa11503507
Umesh Dafda <umeshdafda52@gmail.com>
2026-04-25 19:05:51 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { registerPlugin } from '@capacitor/core';
 import type { AdMobAdvancedPlugin } from './definitions';
 
+const AdMobAdvanced = registerPlugin<AdMobAdvancedPlugin>('AdMobAdvanced', {
+  web: () => import('./web').then(m => new m.AdMobAdvanced()),
+});
+
 export * from './definitions';
-export { AdMobAdvanced } from './web';
+export { AdMobAdvanced };
